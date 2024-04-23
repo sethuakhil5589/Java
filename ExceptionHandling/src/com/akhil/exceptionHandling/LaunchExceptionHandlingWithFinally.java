5130f454d86645bc5927a071d4f6bad31f4d4134
Sethu Akhil <148880835+sethuakhil5589@users.noreply.github.com>
2024-04-23 11:18:49 -0400

    first commit

---
+++ ExceptionHandling/src/com/akhil/exceptionHandling/LaunchExceptionHandlingWithFinally.java
@@ -0,0 +1,49 @@
+package com.akhil.exceptionHandling;
+
+import java.util.Scanner;
+
+public class LaunchExceptionHandlingWithFinally {
+
+	public static void main(String[] args) {
+		int numerator;
+		int denomirator;
+		int result;
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Please enter numerator and denomirator");
+		try {
+		numerator=scan.nextInt();
+		denomirator=scan.nextInt();
+		result=numerator/denomirator;
+		
+		System.out.println("result: "+result);
+		
+		System.out.println("Enter length of array");
+		int lenghtOfArray=scan.nextInt();
+		
+		int []array=new int[lenghtOfArray];
+		
+		System.out.println("Enter place of array");
+		int placeOfArray=scan.nextInt();
+		
+		System.out.println("Enter value of the array to be inserted");
+		int valueOfArray=scan.nextInt();
+		
+		array[placeOfArray]=valueOfArray;
+		}
+		catch(ArithmeticException e){
+			System.out.println("Please dont give zero denomirator");
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Please give value in the bound");
+		}
+		
+		finally {
+			System.out.println("Thank you for using the application");
+		}
+		
+
+
+	}
+
+}
