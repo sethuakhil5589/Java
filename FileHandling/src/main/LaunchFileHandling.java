11d9cd37751ad98e4cc7d2b3320d26c5ea709649
Sethu Akhil <148880835+sethuakhil5589@users.noreply.github.com>
2024-04-24 08:42:28 -0400

    commit

---
+++ FileHandling/src/main/LaunchFileHandling.java
@@ -0,0 +1,40 @@
+package main;
+import java.io.*;
+
+public class LaunchFileHandling {
+
+	public static void main(String[] args) {
+		String path="C:\\Java\\LaunchFileHandling.txt";
+		
+		File file=new File(path);
+		try {
+			System.out.println(file.createNewFile());
+		} catch (Exception e) {
+			System.out.println("some exception occured");
+		}
+		System.out.println(file.isFile());//true
+		
+		System.out.println(file.isDirectory());//false
+		
+		File directory1 =new File("C:\\\\Java\\\\LaunchFileHandling2.");
+		
+		try {
+			System.out.println(directory1.mkdir());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("This is directory "+directory1.isDirectory());
+		
+		
+		File file2=new File("C:\\Java\\");
+		
+		int filesCount=(int)file2.length();
+		
+		System.out.println(filesCount);
+		
+		
+
+	}
+
+}
---
+++ FileHandling/src/main/LaunchFileHandlingReader.java
@@ -0,0 +1,37 @@
+package main;
+
+import java.io.*;
+
+
+public class LaunchFileHandlingReader {
+
+	public static void main(String[] args) throws IOException {
+		FileReader fr=null;
+		String path="C:\\Java\\LaunchFileHandling3";
+		File directory=new File(path);
+		File file=new File("C:\\\\Java\\\\LaunchFileHandling3\\JavaProgramming.txt");
+		directory.mkdir();
+		
+		try {
+			fr=new FileReader(file);
+			int length=(int) file.length();
+			char[] cArray=new char[length];
+			fr.read(cArray);
+//			int i=fr.read();
+			for(int i=0;i<length;i++) {
+				System.out.println(i +"-->"+cArray[i]);
+			}
+			
+			
+		}
+		catch(Exception e) {
+			System.out.println("Some exception occured");
+			e.printStackTrace();
+		}
+		finally {
+			fr.close();
+		}
+
+	}
+
+}
