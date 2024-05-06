ed013123ecddb8fc37e67961c852166b27ee6077
Sethu Akhil <148880835+sethuakhil5589@users.noreply.github.com>
2024-05-06 11:25:44 -0400

    All commits

---
+++ PatternsAndPloymorphism/src/demo/launch/LaunchPriorityDequeue.java
@@ -0,0 +1,13 @@
+package demo.launch;
+
+public class LaunchPriorityDequeue {
+
+	public static void main(String[] args) {
+		/*
+		 * data type= Double indeed Queue
+		 * ArrayDequeue(c)-->Dequeue(I)==>Queue(Extends)
+		 */
+
+	}
+
+}
